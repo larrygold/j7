adda61bf936a6301c65104e13553d993edc4a9c7
ZCLMK <zacharie.NTC@gmail.com>
2018-04-10 22:11:43 +0200

    commit autre ordi

--- zac/benef.rb
+++ zac/benef.rb
@@ -1,6 +1,6 @@
 
     def trader_du_dimanche(array)
-        comparaisons_semaine = []
+        
         index_best_buy_and_sale = Array.new
         index_vente = array.length - 1
         benef_base = 0
@@ -8,25 +8,21 @@
         while index_vente > 0
             prix_reference = array[index_vente]
             puts "jour référence: #{index_vente} "
-            comp_achat = index_vente -1
-            comparaisons_jour = []
+            index_achat = index_vente -1
             
-             
+            
             while index_achat >= 0 
                 benef = array[index_vente] - array[index_achat]
-                index_achat -= 1
-                comparaisons_jour.unshift(benef)
                 if benef > benef_base
+                    benef_base = benef
                     index_best_buy_and_sale[0] = index_achat
                     index_best_buy_and_sale[1] = index_vente
                 end
-
+                index_achat -= 1
             end
-             
-            comparaisons_semaine.push(comparaisons_jour.max)
-            print "benef max semaine =>#{comparaisons_semaine} "
             index_vente -= 1
     end
+    print index_best_buy_and_sale
 end
     trader_du_dimanche([2, 5, 3, 6])
 
