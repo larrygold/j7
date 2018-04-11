9e1d24784e1b0f8e8c32f076e14ec17179ddfbe3
larrygold <larry@goldstein.fr>
2018-04-11 09:58:35 +0200

    .

--- larry/chiffrement.rb
+++ larry/chiffrement.rb
@@ -3,16 +3,22 @@ def chiffre_de_cesar(mystring, nb)
   result = mystring.downcase.split('').map! do |lettre|
 
     i = lettre.ord - 97
-    puts i
 
-      if (i+nb)>25 && i>=0 && i<=25 # si i correspond au code d'une lettre necessitant de boucler 
+      if (i+nb)>25 && i>=0 && i<=25 # si i correspond au code d'une lettre necessitant de boucler
         j = (i+nb)%25-1
       elsif i>=0 && i<=25 #autrement, si i correspond à une lettre ne nécessitant pas de boucler
         j = i+nb
       else # si i ne correspond pas au code d'une lettre
-        return lettre
+        i
       end
   end
   return result.map{|lettre| (lettre.ord+97).chr}.join.capitalize
 end
-print chiffre_de_cesar("abcde mnopqr!", 5)
+
+puts "Veuillez entrer une phrase"
+phrase = gets.chomp
+
+puts "Veuillez entrer un nombre de décalages"
+decalage = gets.chomp.to_i
+
+puts chiffre_de_cesar(phrase, decalage)
